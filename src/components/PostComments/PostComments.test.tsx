9e8a3a2821d6117062b504da99d9cf39f214a64a
EricBortoleto <eric.biazinibortoleto@gmail.com>
2024-11-22 20:52:48 -0300

    teste de dois comentários e atributo data-testid

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,29 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import PostComment from './';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+  it('Deve renderizar o componente corretamente', () => {
+    render(<PostComment />);
+    expect(screen.getByText('Comentar')).toBeInTheDocument();
+  });
+
+  it('Deve adicionar dois comentários', () => {
+    render(<PostComment />);
+
+    fireEvent.change(screen.getByTestId('comment-textarea'), {
+      target: { value: 'Primeiro comentário' },
     });
+
+    fireEvent.submit(screen.getByTestId('comment-form'));
+
+    expect(screen.getByText('Primeiro comentário')).toBeInTheDocument();
+
+    fireEvent.change(screen.getByTestId('comment-textarea'), {
+      target: { value: 'Segundo comentário' },
+    });
+
+    fireEvent.submit(screen.getByTestId('comment-form'));
+
+    expect(screen.getByText('Segundo comentário')).toBeInTheDocument();
+  });
 });
